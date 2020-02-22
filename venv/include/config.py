c6aa466e44272cb9cdeccc5d433bc96c2aa7811e
Sergey Chabanov <chabanovsn@mail.ru>
2020-02-22 03:55:35 +0300

    убрал магические цифры и разбил на функции

---
+++ venv/include/config.py
@@ -0,0 +1,20 @@
+# size of main screen
+surface_width = 800
+surface_height = 600
+# позиция карт противника
+cartPosCompY = 11
+# позиция карт противника
+cartPosUserY = 503
+# по горизонтали одинаково
+cartPos1X = 300
+cartPos2X = 351
+cartPos3X = 402
+cartPos4X = 453
+
+# size of card
+sizeCardW = 50
+sizeCarH = 80
+# height of towers
+maxheight = 150
+heightC = 150
+heightU = 150
